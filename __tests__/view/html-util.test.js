2d9b30474f1b7843eb2ec10bed26e8dcd5b59b5a
小野裕輔 <y-ono@nswt.co.jp>
2022-05-05 07:52:11 +0900

    escapeSpecialChars関数のユニットテストを追加する

--- __tests__/view/html-util.test.js
+++ __tests__/view/html-util.test.js
@@ -1,8 +1,12 @@
-import { escapeSpecialChars } from "../../src/view/html-util.js";
+// Jestによる単体テスト（ユニットテスト）の例
+// cf. https://jestjs.io/ja/docs/getting-started
+import { escapeSpecialChars } from "../../src/view/html-util.js"; // テスト対象の関数を指定する
 
 // escapeSpecialChars関数のテスト
 describe("escapeSpecialCharsのテスト", () => {
+    // test関数は、第1引数にテスト名称を記載し、第2引数にテストを記述する。
     test("'&' を '&amp;' に 変換するテスト", () => {
+        // expect関数の引数にテスト対象の処理を記述し、マッチャー( toBe toEqual など)で期待する動作を検証する。
         expect(escapeSpecialChars("&")).toBe("&amp;");
     });
     test("'<' を '&lt;' に 変換するテスト", () => {
