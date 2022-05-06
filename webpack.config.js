7e420c12d19ed751d33140e70affa923815619e2
小野裕輔 <y-ono@nswt.co.jp>
2022-05-06 16:51:17 +0900

    webpack管理とするためにsrcディレクトリ配下に移動する

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// プラグインの読み込み
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    // "plugins" エントリーを追加
+    plugins: [
+        // プラグインのインスタンスを作成
+        new HtmlWebpackPlugin({
+            // テンプレート
+            template: "./src/index.html",
+            // <script> ~ </script> タグの挿入位置
+            inject: "body",
+            // スクリプト読み込みのタイプ
+            scriptLoading: "defer",
+            // ファビコンも <link rel="shortcut icon" ~ /> として挿入できる
+            // favicon: "./src/favicon.ico",
+        }),
+    ],
+};
