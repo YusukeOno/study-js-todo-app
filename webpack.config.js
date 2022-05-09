7e50b95c65fa93cb7aa38bd42c4f60773e3ef7d3
小野裕輔 <y-ono@nswt.co.jp>
2022-05-09 16:36:28 +0900

    scriptタグが自動挿入されないようにする。

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 // プラグインの読み込み
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
     // production or development
     mode: "development",
+    // ここは絶対パスで指定する必要がある。
+    output: {
+        // path: `${__dirname}/dist`,
+        path: path.resolve(__dirname, "./dist"),
+        filename: "index.js",
+    },
     // "plugins" エントリーを追加
     plugins: [
         // プラグインのインスタンスを作成
@@ -11,7 +18,7 @@ module.exports = {
             // テンプレート
             template: "./src/index.html",
             // <script> ~ </script> タグの挿入位置
-            inject: "body",
+            inject: false,
             // スクリプト読み込みのタイプ
             scriptLoading: "defer",
             // ファビコンも <link rel="shortcut icon" ~ /> として挿入できる
